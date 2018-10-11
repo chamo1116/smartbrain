c929fc02f06dd7024dcbcb131c2c258c8de27677
Joan Sebastian Chamorro <joan.chamorro1116@gmail.com>
2018-10-10 20:48:42 -0500

    first commit

---
+++ Controllers/image.js
@@ -0,0 +1,33 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '53263dcb5fe2417a9ad2dc36f923c37f'
+});
+
+
+const handleApiCall = (req, res) => {
+	app.models
+    	.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    	.then(data => {
+    		res.json(data)
+    	})
+    	.catch(err => res.status(400).json('Unable to work with API'))
+}
+
+
+const handleImage = (req, res, db)=>{
+	const { id }= req.body;
+	let found = false;
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0])
+	})
+	.catch(err => res.status(400).json('Enable to get entries'))
+}
+
+module.exports = {
+	handleImage: handleImage,
+	handleApiCall: handleApiCall
+}
