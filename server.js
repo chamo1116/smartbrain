ffe09850502e47665d91d0332f80dc97998dc1eb
Joan Sebastian Chamorro <joan.chamorro1116@gmail.com>
2018-10-10 21:37:06 -0500

    Updating database

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require ('./Controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-parallel-91041',
     user : 'postgres',
     password : 'chamito',
     database : 'smartbrain'
